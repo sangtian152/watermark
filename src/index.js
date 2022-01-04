daafa04ecd40d7fc99d78c65b2ff4dd229aaaaeb
sangtian152 <jzyazn@sina.cn>
2022-01-04 16:56:34 +0800

    修改package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import canvasImage from './canvas'
-const image2canvas = function (src, options) {
+const watermark = function (src, options) {
     const _canvas = new canvasImage(src)
     return _canvas.init(options)
 }
-export default image2canvas
+export default watermark
