3a47b02405ebc3013dc20022b28a75d80aef1bc8
sangtian152 <jzyazn@sina.cn>
2022-01-06 13:59:27 +0800

    修改package.json

--- README.md
+++ README.md
@@ -15,7 +15,9 @@ yarn add @sangtian152/watermark
 import watermark from '@sangtian152/watermark';
 
 const can = new watermark(url, options);
-can.addText('测试水印居中', {repeat: false, position: {right: '50%', bottom:  '50%'}, translate: '50%,50%'})
+can.addText('文字水印', {repeat: true, lineSpacing: 146, markSpacing: '50%' })
+can.addText('测试水印居中', {repeat: false, fontSize: 36, position: {right: '50%', bottom:  '50%'}, translate: '50%,50%'})
+can.addImage(mark, {repeat: true, markHeight: 26, lineSpacing: 146, start: 36})
 can.draw(function() {
     console.log('水印设置成功')
     const img = can.getBase64()
--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,4 @@
-import watermark from 'src/index'
+import watermark from 'watermark'
 const src = require('src/static/demo.jpg').default
 const mark = require('src/static/mark.png').default
 
@@ -8,8 +8,8 @@ window.onload = function() {
 document.getElementById('create').onclick = function(){
     const ele = document.getElementById('img')
     const can = new watermark(src, {fontSize: 26, fillStyle: '#fff', globalAlpha: 0.5})
-    can.addText('水印', {repeat: true, lineSpacing: 146, markSpacing: '50%' })
-    can.addText('测试水印', {repeat: false, position: {right: '50%', bottom:  '50%'}, translate: '50%,50%'})
+    can.addText('文字水印', {repeat: true, lineSpacing: 146, markSpacing: '50%' })
+    can.addText('测试水印居中', {repeat: false, fontSize: 36, position: {right: '50%', bottom:  '50%'}, translate: '50%,50%'})
     can.addImage(mark, {repeat: true, markHeight: 26, lineSpacing: 146, start: 36})
     can.draw(function() {
       console.log('canvas.draw')
